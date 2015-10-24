1faaf5fa025746dccb600eb705b0861d9b04e5fa
RaviDesai <ravidesai@me.com>
2015-10-24 13:08:06 -0400

    Initial commit

---
+++ Example/RSDTesting/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  RSDTesting
+//
+//  Created by RaviDesai on 10/24/2015.
+//  Copyright (c) 2015 RaviDesai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
